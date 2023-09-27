9b982e478b7f9760519f7ac075908a962220fbb3
librankaio <95515543+librankaio@users.noreply.github.com>
2023-09-27 14:39:28 +0700

    change barcode print item 3 27092023

--- resources/views/pages/Print/tsuratjalanprintitem.blade.php
+++ resources/views/pages/Print/tsuratjalanprintitem.blade.php
@@ -28,7 +28,13 @@
 <body>
 @foreach($items as $item)
   @for($i = 0; $i < $item->qty; $i++)
-  <div class="container" style="padding-bottom: 10px;">
+  <div class="container" style="padding-bottom: 4px;">
+    <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name }}</h5>
+    <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/> <br>
+    <h1 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h1></span></h1>
+    <h1 style="margin: 0px auto; text-align: center; font-size: 9px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
+  </div>
+  <div class="container" style="padding-bottom: 4px;">
     <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name }}</h5>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/> <br>
     <h1 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h1></span></h1>
