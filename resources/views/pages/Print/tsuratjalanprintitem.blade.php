e1a10dea8a98589e2d49fcc86b9b50ef4aa83404
librankaio <95515543+librankaio@users.noreply.github.com>
2023-09-27 14:55:53 +0700

    change barcode print item 6 27092023

--- resources/views/pages/Print/tsuratjalanprintitem.blade.php
+++ resources/views/pages/Print/tsuratjalanprintitem.blade.php
@@ -28,16 +28,16 @@
 <body>
 @foreach($items as $item)
   @for($i = 0; $i < $item->qty; $i++)
-  <div class="container" style="padding-bottom: 6px;">
-    <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name }}</h5>
+  <div class="container" style="padding-bottom: 9px;">
+    <h5 style="margin: 0px auto; font-size: 9px;" id="title">{{ $item->name }}</h5>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/> <br>
-    <h1 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h1></span></h1>
+    <h1 class="split-para" style="margin: 0px auto; font-size: 9px; text-align:left;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h1></span></h1>
     <h1 style="margin: 0px auto; text-align: center; font-size: 9px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
   </div>
   <div class="container" style="padding-bottom: 6px;">
-    <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name }}</h5>
+    <h5 style="margin: 0px auto; font-size: 9px;" id="title">{{ $item->name }}</h5>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/> <br>
-    <h1 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h1></span></h1>
+    <h1 class="split-para" style="margin: 0px auto; font-size: 9px; text-align:left;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h1></span></h1>
     <h1 style="margin: 0px auto; text-align: center; font-size: 9px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
   </div>
   @endfor
