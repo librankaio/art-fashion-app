53a6dc39dfd15f459933439bad20ed65f05039ad
librankaio <95515543+librankaio@users.noreply.github.com>
2023-11-27 16:16:19 +0700

    update print barcode mitemsuratjalan v2 27112023

--- resources/views/pages/Print/tsuratjalanprintitem.blade.php
+++ resources/views/pages/Print/tsuratjalanprintitem.blade.php
@@ -114,6 +114,7 @@
     { 
       word-wrap: break-word
     } 
+    .page_break { page-break-before: always; }
   </style>
 </head>
 </html>
@@ -122,19 +123,20 @@
   @for($i = 0; $i < $item->qty; $i++)
   <div class="container" style="padding-bottom: 7px;">
     {{-- <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name." - ".$item->warna }}</h5> --}}
-    <h5 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->name_lbl }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right; margin-top:1.5px">{{ $item->warna }}</h5></span></h5>
+    <h5 class="split-para" style="margin: 0px auto; font-size: 6px; text-align:left;" id="text_code">{{ $item->name_lbl }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right; margin-top:1.5px">{{ $item->warna }}</h5></span></h5>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/>
-    <h1 class="split-para" style="margin: 0px auto; font-size: 6px; text-align:left; margin: 0px auto;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right; margin: 0px auto;">{{date("mY")}}</h1></span></h1>
-    <h1 style="margin: 0px auto; text-align: center; font-size: 8px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
+    <h5 class="split-para" style="margin: 0px auto; font-size: 6px; text-align:left;" id="text_code">{{ $item->code }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h5></span></h5>
+    <h1 style="margin: 0px auto; text-align: center; font-size: 7px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
   </div>
   <div class="container">
   {{-- <div class="container" style="padding-bottom: 7px;"> --}}
     {{-- <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name." - ".$item->warna}}</h5> --}}
-    <h5 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->name_lbl }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right; margin-top:1.5px">{{ $item->warna }}</h5></span></h5>
-    <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg" style="margin: 0px auto;"/>
-    <h1 class="split-para" style="margin: 0px auto; font-size: 6px; text-align:left; margin: 0px auto;" id="text_code">{{ $item->code }} <span><h1 style="margin: 0px auto; font-size: 6px; float:right; margin: 0px auto;">{{date("mY")}}</h1></span></h1>
-    <h1 style="margin: 0px auto; text-align: center; font-size: 8px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
+    <h5 class="split-para" style="margin: 0px auto; font-size: 6px; text-align:left;" id="text_code">{{ $item->name_lbl }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right; margin-top:1.5px">{{ $item->warna }}</h5></span></h5>
+    <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/>
+    <h5 class="split-para" style="margin: 0px auto; font-size: 6px; text-align:left;" id="text_code">{{ $item->code }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right;">{{date("mY")}}</h5></span></h5>
+    <h1 style="margin: 0px auto; text-align: center; font-size: 7px;">RP. {{ number_format($item->hrgjual, 2, '.', ',') }},-</h1>
   </div>
+  <div class="page_break"></div> 
   @endfor
 @endforeach
 </body>
