8ff7fdbf84a3205c81021df92810bc943064a4ae
librankaio <95515543+librankaio@users.noreply.github.com>
2023-11-27 13:55:12 +0700

    update barcode 2 suratjalanitem 27112023

--- resources/views/pages/Print/tsuratjalanprintitem.blade.php
+++ resources/views/pages/Print/tsuratjalanprintitem.blade.php
@@ -120,7 +120,7 @@
 <body>
 @foreach($items as $item)
   @for($i = 0; $i < $item->qty; $i++)
-  <div class="container" style="padding-bottom: 7px; padding-top:3px">
+  <div class="container" style="padding-bottom: 7px;">
     {{-- <h5 style="margin: 0px auto; font-size: 8px;" id="title">{{ $item->name." - ".$item->warna }}</h5> --}}
     <h5 class="split-para" style="margin: 0px auto; font-size: 8px; text-align:left;" id="text_code">{{ $item->name_lbl }} <span><h5 style="margin: 0px auto; font-size: 6px; float:right; margin-top:1.5px">{{ $item->warna }}</h5></span></h5>
     <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($item->code , 'C128') }}" alt="barcode" width="100" height="20" id="bgimg"/> <br>
