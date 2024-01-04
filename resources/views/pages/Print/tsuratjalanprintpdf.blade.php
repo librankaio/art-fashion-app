580b1bd9dfbc0f8d742294d9503491ea2290f9c7
librankaio <95515543+librankaio@users.noreply.github.com>
2024-01-04 14:51:45 +0700

    update suratjalanpdf v1 04012024

--- resources/views/pages/Print/tsuratjalanprintpdf.blade.php
+++ resources/views/pages/Print/tsuratjalanprintpdf.blade.php
@@ -6,7 +6,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <style>
-    
+    @page {
+    margin-top: 0px;
+    }
     body {
         /* font-family: 'Roboto';font-size: 16px; */
         font-family: 'Roboto', sans-serif;
