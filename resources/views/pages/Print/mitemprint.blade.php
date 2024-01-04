4f940c63eb1a1cb28edd341d282604a4cb9af86a
librankaio <95515543+librankaio@users.noreply.github.com>
2024-01-04 11:12:49 +0700

    update font v10 04012023

--- resources/views/pages/Print/mitemprint.blade.php
+++ resources/views/pages/Print/mitemprint.blade.php
@@ -1,90 +1,10 @@
 <html>
   <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <style>
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Bold.ttf") }}) format("truetype");
-        font-weight: 700;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-BoldItalic.ttf") }}) format("truetype");
-        font-weight: 700;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-ExtraBold.ttf") }}) format("truetype");
-        font-weight: 800;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-ExtraBoldItalic.ttf") }}) format("truetype");
-        font-weight: 800;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Light.ttf") }}) format("truetype");
-        font-weight: 300;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-LightItalic.ttf") }}) format("truetype");
-        font-weight: 300;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Medium.ttf") }}) format("truetype");
-        font-weight: 500;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-MediumItalic.ttf") }}) format("truetype");
-        font-weight: 500;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Regular.ttf") }}) format("truetype");
-        font-weight: 400;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-SemiBold.ttf") }}) format("truetype");
-        font-weight: 600;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-SemiBoldItalic.ttf") }}) format("truetype");
-        font-weight: 600;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Italic.ttf") }}) format("truetype");
-        font-weight: 400;
-        font-style: italic;
-    }
     @page {
       size: 30mm 33mm;
       margin: -1px auto;
@@ -107,7 +27,7 @@
       display:block; float:right; padding-right:7px; padding-top:0px;
     } */
     body {
-        font-family: 'Open Sans', sans-serif;
+        font-family: 'Roboto', sans-serif;
     }
     p  
     { 
--- resources/views/pages/Print/tsuratjalanprintpdf.blade.php
+++ resources/views/pages/Print/tsuratjalanprintpdf.blade.php
@@ -2,99 +2,20 @@
 <html>
   <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
   <style>
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Bold.ttf") }}) format("truetype");
-        font-weight: 700;
-        font-style: normal;
-    }
     
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-BoldItalic.ttf") }}) format("truetype");
-        font-weight: 700;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-ExtraBold.ttf") }}) format("truetype");
-        font-weight: 800;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-ExtraBoldItalic.ttf") }}) format("truetype");
-        font-weight: 800;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Light.ttf") }}) format("truetype");
-        font-weight: 300;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-LightItalic.ttf") }}) format("truetype");
-        font-weight: 300;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Medium.ttf") }}) format("truetype");
-        font-weight: 500;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-MediumItalic.ttf") }}) format("truetype");
-        font-weight: 500;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Regular.ttf") }}) format("truetype");
-        font-weight: 400;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-SemiBold.ttf") }}) format("truetype");
-        font-weight: 600;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-SemiBoldItalic.ttf") }}) format("truetype");
-        font-weight: 600;
-        font-style: italic;
-    }
-    
-    @font-face {
-        font-family: 'Open Sans';
-        src: url({{ storage_path("fonts/static/OpenSans/OpenSans-Italic.ttf") }}) format("truetype");
-        font-weight: 400;
-        font-style: italic;
-    }
     body {
         /* font-family: 'Roboto';font-size: 16px; */
-        font-family: 'Open Sans', sans-serif;
+        font-family: 'Roboto', sans-serif;
     }
     h1,h2,h3,h4,h5,h6 {
-        font-family: 'Open Sans', sans-serif;
+      font-family: 'Roboto', sans-serif;
     }
     td{
-        font-family: 'Open Sans';
+        font-family: 'Roboto', sans-serif;
         font-weight: bold;
     }
     hr{
